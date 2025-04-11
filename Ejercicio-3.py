2d02b73bdb328d511a290e5b2f9db58eefb33db4
franco <166817040+22franco@users.noreply.github.com>
2025-04-10 23:15:23 -0300

    Update Ejercicio-3.py

--- Ejercicio-3.py
+++ Ejercicio-3.py
@@ -9,42 +9,42 @@ class Punto:
 
 class Linea:
     def __init__(self, punto_a, punto_b):
-        # guarda los dos puntos que forman la línea
+        # guarda los dos puntos que forman la linea
         self._punto_a = punto_a
         self._punto_b = punto_b
 
               
     def mueve_derecha(self, distancia):
-        # mueve la línea a la derecha sumando a x
+        # mueve la linea a la derecha sumando a x
         self._punto_a.x += distancia
         self._punto_b.x += distancia
      
     def mueve_izquierda(self, distancia):
-        # mueve la línea a la izquierda restando a x
+        # mueve la linea a la izquierda restando a x
         self._punto_a.x -= distancia
         self._punto_b.x -= distancia
         
     def mueve_arriba(self, distancia):
-        # mueve la línea hacia arriba sumando a y
+        # mueve la linea hacia arriba sumando a y
         self._punto_a.y += distancia
         self._punto_b.y += distancia
         
     def mueve_abajo(self,distancia):
-        # mueve la línea hacia abajo restando a y
+        # mueve la linea hacia abajo restando a y
         self._punto_a.y -= distancia
         self._punto_b.y -= distancia
         
         
     
     def __str__(self):
-        # muestra la línea como texto con sus dos puntos
+        # muestra la liinea como texto con sus dos puntos
         return f"Línea de {self._punto_a} a {self._punto_b}"
     
-# se crean dos puntos y una línea entre ellos
+# se crean dos puntos y una linea entre ellos
 punto1 = Punto(1,2)
 punto2 = Punto(3,4)
 
-# se mueve la línea en diferentes direcciones
+# se mueve la linea en diferentes direcciones
 linea = Linea(punto1,punto2)
 print(linea)
 
